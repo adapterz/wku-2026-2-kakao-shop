f96822b8528577256fa35035be8b53f944dc3021
mikuuu3 <dlcoduud@gmail.com>
2026-07-09 14:10:41 +0900

    feat: 상품 상세 페이지 구현

---
+++ public/js/product.js
@@ -0,0 +1,10 @@
+// API 명세서의 3-1, 3-2를 참고하여 작성한 임시 상품 더미 데이터
+const mockProduct = {
+  id: 1,
+  name: "익산역 아메리카노 교환권",
+  brand: "익산역점",
+  price: 4500,
+  thumbnailUrl: "https://example.com/images/coffee.jpg",
+  description: "익산역 인근 카페에서 이용 가능한 커피 교환권입니다.",
+  usageInfo: "발급일로부터 30일 이내 사용 가능합니다."
+};
