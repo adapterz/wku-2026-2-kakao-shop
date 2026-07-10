90b3358f153bd57eb00d93030519bd0ba9f8169c
mikuuu3 <dlcoduud@gmail.com>
2026-07-11 05:07:34 +0900

    사진 저장하기 버튼명 수정 : 선물 사용하기

--- public/js/giftuse.js
+++ public/js/giftuse.js
@@ -64,7 +64,7 @@ document.addEventListener('DOMContentLoaded', async () => {
             console.error(err);
             alert("사용 처리에 실패했습니다. 서버 API 구현 여부를 확인해주세요.");
             btnUse.disabled = false;
-            btnUse.textContent = "내 앨범에 저장";
+            btnUse.textContent = "선물 사용하기";
         }
     });
 });
