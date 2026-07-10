b6cd772004eb73ca66ae16dda6dfe229ddd3c717
mikuuu3 <dlcoduud@gmail.com>
2026-07-10 14:18:51 +0900

    선물함 만드는중

--- db/models/giftModel.js
+++ db/models/giftModel.js
@@ -7,10 +7,12 @@ const getGiftsByReceiverId = async (receiverId, status) => {
       g.id as gift_id,
       p.name as product_name,
       p.thumbnail_url as thumbnail_url,
+      p.brand as brand,
       g.status as status,
       u.nickname as sender_nickname,
       o.is_self_gift as is_self_gift,
-      g.created_at as created_at
+      g.created_at as created_at,
+      g.used_at as used_at
     FROM gifts g
     JOIN orders o ON g.order_id = o.id
     JOIN products p ON o.product_id = p.id
